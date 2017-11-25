def9b2aaa542a7d6597df6df72c5c9fe24871e98
Thanos Mavroidis <thanosmavroidis@Thanoss-MBP.home>
2017-11-25 21:47:38 +0000

    Added abstract activity for state update presentation

---
+++ app/src/main/java/uk/co/akm/test/sim/boatinpond/activity/AbstractStateActivity.java
@@ -0,0 +1,78 @@
+package uk.co.akm.test.sim.boatinpond.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+import uk.co.akm.test.sim.boatinpond.game.StateProcessor;
+import uk.co.akm.test.sim.boatinpond.game.StateUpdateLoop;
+import uk.co.akm.test.sim.boatinpond.game.impl.StateUpdateLoopThread;
+import uk.co.akm.test.sim.boatinpond.phys.UpdatableState;
+
+/**
+ * Created by Thanos Mavroidis on 25/11/2017.
+ */
+public abstract class AbstractStateActivity<T extends UpdatableState, G> extends AppCompatActivity implements StateProcessor<T, G>, StateUpdateLoop {
+    private final long uiUpdateMillis;
+    private final int nUpdateSteps;
+    private final RenderRunnable<G> renderRunnable;
+
+    private T state;
+    private StateUpdateLoop stateUpdateLoop;
+
+    protected AbstractStateActivity(long uiUpdateMillis, int nUpdateSteps) {
+        this.uiUpdateMillis = uiUpdateMillis;
+        this.nUpdateSteps = nUpdateSteps;
+        this.renderRunnable = new RenderRunnable<G>(this);
+    }
+
+    protected final void initState(T state) {
+        this.state = state;
+    }
+
+    @Override
+    public final T getStateReference() {
+        return state;
+    }
+
+    @Override
+    public final void initiate() {
+        stateUpdateLoop = new StateUpdateLoopThread<T, G>(this, uiUpdateMillis, nUpdateSteps);
+        stateUpdateLoop.initiate();
+    }
+
+    @Override
+    public final void renderState(G renderingData) {
+        renderRunnable.setRenderingData(renderingData);
+        runOnUiThread(renderRunnable);
+    }
+
+    protected abstract void drawState(G renderingData);
+
+    @Override
+    public final void terminate() {
+        if (stateUpdateLoop != null) {
+            stateUpdateLoop.terminate();
+            stateUpdateLoop = null;
+        }
+    }
+
+    private static final class RenderRunnable<G> implements Runnable {
+        private final AbstractStateActivity<?, G> parent;
+
+        private G renderingData;
+
+        RenderRunnable(AbstractStateActivity<?, G> parent) {
+            this.parent = parent;
+        }
+
+        void setRenderingData(G renderingData) {
+            this.renderingData = renderingData;
+        }
+
+        @Override
+        public void run() {
+            if (renderingData != null) {
+                parent.drawState(renderingData);
+            }
+        }
+    }
+}
--- app/src/main/java/uk/co/akm/test/sim/boatinpond/game/impl/StateUpdateLoopThread.java
+++ app/src/main/java/uk/co/akm/test/sim/boatinpond/game/impl/StateUpdateLoopThread.java
@@ -10,19 +10,32 @@ import uk.co.akm.test.sim.boatinpond.phys.UpdatableState;
  * Created by Thanos Mavroidis on 20/11/2017.
  */
 public final class StateUpdateLoopThread<T extends UpdatableState, G> extends Thread implements StateUpdateLoop {
-    private final long dt;
-    private final long dtMillis;
+    private final double dt;
+    private final long uiUpdateMillis;
+    private final int nUpdateSteps;
+
     private final StateProcessor<T, G> stateProcessor;
 
     private final T state;
 
     private boolean loop;
 
-    public StateUpdateLoopThread(StateProcessor<T, G> stateProcessor, long dt) {
-        this.dt = dt;
-        this.dtMillis = 1000*dt;
+    /**
+     *
+     * @param stateProcessor
+     * @param uiUpdateMillis the time interval, in milliseconds, in which the state is rendered in the UI
+     * @param nUpdateSteps the number of state updates to be calculated internally for the UI update time interval
+     */
+    public StateUpdateLoopThread(StateProcessor<T, G> stateProcessor, long uiUpdateMillis, int nUpdateSteps) {
+        this.uiUpdateMillis = uiUpdateMillis;
+        this.nUpdateSteps = nUpdateSteps;
+        this.dt = (uiUpdateMillis/1000.0)/nUpdateSteps;
         this.stateProcessor = stateProcessor;
         this.state = stateProcessor.getStateReference();
+
+        if (state == null) {
+            throw new IllegalArgumentException("Cannot construct a " + getClass().getSimpleName() + " instance with a " + StateProcessor.class.getSimpleName() + " instance that gives a null state. It is not possible to update a null state!");
+        }
     }
 
     @Override
@@ -44,31 +57,42 @@ public final class StateUpdateLoopThread<T extends UpdatableState, G> extends Th
 
     private void loopStep(T state) {
         final long startMillis = System.currentTimeMillis();
-        state.update(dt);
+        updateState(state);
         final G renderingData = stateProcessor.computeRenderingData(state);
         waitIfRequired(startMillis);
 
-        stateProcessor.renderState(renderingData);
+        if (loop) {
+            stateProcessor.renderState(renderingData);
+        }
+    }
+
+    private void updateState(T state) {
+        for (int i=0 ; i<nUpdateSteps ; i++) {
+            state.update(dt);
+        }
     }
 
     private void waitIfRequired(long startMillis) {
         final long evalMillis = (System.currentTimeMillis() - startMillis);
-        final long waitMillis = (dtMillis - evalMillis);
+        final long waitMillis = (uiUpdateMillis - evalMillis);
         if (waitMillis > 0) {
             waitFor(waitMillis);
         } else if (waitMillis < 0) {
-            Log.d(getClass().getSimpleName(), "Loop step time exceeded update time (" + dtMillis + " millis) by " + (-waitMillis) + " millis.");
+            Log.d(getClass().getSimpleName(), "Loop step time exceeded update time (" + uiUpdateMillis + " millis) by " + (-waitMillis) + " millis.");
         }
     }
 
     private void waitFor(long waitMillis) {
         try {
             Thread.sleep(waitMillis);
-        } catch (InterruptedException ignore) {}
+        } catch (InterruptedException ie) {
+            loop = false;
+        }
     }
 
     @Override
     public void terminate() {
         loop = false;
+        interrupt();
     }
 }
