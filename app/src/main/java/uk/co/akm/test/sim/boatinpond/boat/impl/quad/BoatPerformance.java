5bd8659f5cf217e3000cb0f32361ee0561d7eaa6
Thanos Mavroidis <thanos.mavroidis@travelex.com>
2018-03-14 10:56:13 +0000

    Added comments.

--- app/src/main/java/uk/co/akm/test/sim/boatinpond/boat/impl/quad/BoatPerformance.java
+++ app/src/main/java/uk/co/akm/test/sim/boatinpond/boat/impl/quad/BoatPerformance.java
@@ -1,13 +1,40 @@
 package uk.co.akm.test.sim.boatinpond.boat.impl.quad;
 
 /**
+ * Defines the linear motion and turning motion boat performance by specifying parameters for 2 scenarios:
+ *
+ * 1) The boat is launched with an initial speed V0 and is let to slow down by the water resistance.
+ *    As a result there will be a theoretical distance D which the boat will always approach but never
+ *    reach. The parameters V0 and D are used to define the linear motion performance characteristics.
+ *
+ * 2) The boat is turning at constant speed V describing a circle with radius R. This is with enough
+ *    motor power to sustain a constant speed and under full rudder deflection. The turn rate A is
+ *    equal to A = 2*pi*R/T where T is the time it takes to complete one circle (i.e. T = 2*pi*R/V).
+ *    The parameters V and A are used to define the turning motion performance characteristics.
+ *
  * Created by Thanos Mavroidis on 25/02/2018.
  */
 public class BoatPerformance {
+    /**
+     * The initial speed with which the boat must be launched to be able to (almost) traverse the distance defined by {@link #distanceLimit}
+     */
     public final double launchSpeed;
+
+    /**
+     * The distance that the boat will be able to (almost) traverse if launched with an initial speed defined by {@link #launchSpeed}
+     */
     public final double distanceLimit;
 
+
+
+    /**
+     * The sustained turn rate that the boat will achieve will full rudder deflection when travelling at a constant speed defined by {@link #turningSpeed}
+     */
     public final double turnRate;
+
+    /**
+     * The constant speed that the boat will be travelling when constantly turning, under full rudder deflection, with a turn rate defined in {@link #turnRate}
+     */
     public final double turningSpeed;
 
     public BoatPerformance(double launchSpeed, double distanceLimit, double turnRate, double turningSpeed) {
